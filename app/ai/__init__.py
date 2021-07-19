15cba4b1983e6dfd27deed32227eaa08c12af681
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-07-18 18:58:11 -0700

    add app/ai & app/data configs

---
+++ app/ai/__init__.py
@@ -0,0 +1,9 @@
+from django.apps.config import AppConfig
+
+
+class AI(AppConfig):
+    name = 'ai'
+
+    label = 'AI'
+
+    verbose_name = 'AI'
---
+++ app/data/__init__.py
@@ -0,0 +1,9 @@
+from django.apps.config import AppConfig
+
+
+class DataMgt(AppConfig):
+    name = 'data'
+
+    label = 'DataMgt'
+
+    verbose_name = 'Data Management'
