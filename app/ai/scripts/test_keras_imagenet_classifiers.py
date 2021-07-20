5fb962f5fd69e191ddf8856d3a71f6b0278f7ed2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-07-20 16:32:34 -0700

    update app.ai.scripts.test_keras_imagenet_classifiers

--- app/ai/scripts/test_keras_imagenet_classifiers.py
+++ app/ai/scripts/test_keras_imagenet_classifiers.py
@@ -3,8 +3,8 @@ from ..models import KerasImageNetClassifier
 from tqdm import tqdm
 
 
-def run():
+def run(image_file_path: str):
     for model in tqdm(KerasImageNetClassifier.objects.all(),
                       total=KerasImageNetClassifier.objects.count()):
         print(model)
-        print(model.predict('/home/project/test.jpg'))
+        print(model.predict(image_file_path=image_file_path, n_labels=5))
